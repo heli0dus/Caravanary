456754d858c6b0993312b7e8bd50ff7bf0b8d7df
Zombe2203 <zombe2203@gmail.com>
2020-05-24 22:38:16 +0300

    Requests.py added Unit types added to Requests

---
+++ Requests.py
@@ -0,0 +1,52 @@
+# Things that need to do
+#
+# Mercenarys:
+# Employee:               high:   evade
+#                         low:    hp, armor, accuracy
+#                         ext.low: damage
+# (chance to skip turn)
+# Rookie - mercenary:     high:   none
+#                         low:    everything
+# Defender - mercenary:   high:   hp, armor
+#                         low:    damage, evade, accuracy
+# ??? - mercenary:        high:   damage, accuracy
+#                         low:    hp, armor, evade
+# Solder - mercenary:     high:   hp, accuracy
+#                         low:    damage, armor, evade
+# Guardian - mercenary:   high:   hp, accuracy, armor
+#                         low:    damage, evade
+# Assassin - mercenary:   high:   damage, accuracy, evade
+#                         low:    hp, armor
+# Hero - mercenary:       high:   hp, damage, evade
+#                         low:    armor, accuracy
+# Ultimate - mercenary:   high:   everything
+#                         low:    none
+
+# Bandits:
+# Thief - bandit:         high: none
+#                         low: everything
+# Pickpoket - bandit:     high: accuracy, evade
+#                         low: hp, damage, armor
+# Archer - bandit:        high: damage, accuracy
+#                         low: hp, armor, evade
+# Bouncer - bandit:       high: hp, damage
+#                         low: armor, evade, accuracy
+# Deserter - bandit:      high: hp, accuracy, armor
+#                         low: evade, damage
+# Killer - bandit:        high: damage, accuracy, evade
+#                         low: hp, armor
+# Leader - bandit:        high: hp, armor, evade, accuracy
+#                         low: damage
+
+# Slaves:
+# Worker - slave:         high: hp
+#                         low: accuracy, armor, evade
+#                         ext.low: damage
+# Hercules - slave:       high: hp, damage
+#                         low: accuracy, armor, evade
+# Gladiator - slave:      high: accuracy, evade
+#                         low: damage, armor, hp
+# Arena Hero - slave:     high: hp, accuracy, evade
+#                         low: damage, armor
+# Hippo - slave:          high: hp, damage, armor
+#                         low: evade, accuracy
